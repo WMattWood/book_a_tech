3a714f8942cdc31c37ee179669258b49ee101ba8
Matt Wood <w.matthew.wood@gmail.com>
2022-05-25 15:19:29 -0400

    Added top level sketch/pseudocode for a billing system.

---
+++ billing_system/pay_period_module.rb
@@ -0,0 +1,18 @@
+# user input
+# period cycle
+  # number of days
+  # number of hours ??? might be extreme
+  # preset week/month modules based on a ruby calendar library
+  # DEFAULT - MONTH
+# start date
+  # client anniversary (ie. day client signed up)
+    # if client signed up on the 31st maybe push it to the 1st or 30th
+  # first day of the month
+  # arbitrary start date
+  # Maybe every 2,3,4 weeks instead of basing it on month days? 
+    # divide the billing year into 
+# periodic charge
+  # should have a method to derive the prorated cost
+  # method to charge to user billing account
+# usage based charge
+  # optional - maybe not used for say... techs.
